23a691aaaa1617e95640315145d6ca6326cf5ab1
PT-10 <tandonprashant101@gmail.com>
2024-04-12 10:50:35 +0530

    CORE: Updated .gitignore and removed __pycache__ from git
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from agent import Agent
 from ddqn_agent import DDQN_Agent
 
 if __name__ == "__main__":
-    #agent = Agent(useGPU=True, useDepth=True)
-    #agent.train()
-    ddqn_agent = DDQN_Agent(useDepth=False)
-    ddqn_agent.train()
+    agent = Agent(useGPU=True, useDepth=True)
+    agent.train()
+    # ddqn_agent = DDQN_Agent(useDepth=False)
+    # ddqn_agent.train()
